d1fc55f61a28ebf6515ccff59f82754ff0623da4
Olaniran Azeez <azeezolaniran2016@gmail.com>
2016-10-28 00:18:44 +0100

    Fixed bug causing user module to break

--- user.js
+++ user.js
@@ -1,23 +1,16 @@
-/*const requireReadLine = require("readline");
-const readline = requireReadLine.createInterface({
-  input: process.stdin , output: process.stdout
-});
-*/
 //User Object
 var User = function (firstName, lastName) {
-  this.firstName = firstName ; //user first name
-  this.lastName = lastName;  //user last name
+  this.firstName = firstName === undefined ? "": firstName ; //user first name
+  this.lastName = lastName === undefined ? "" : lastName;  //user last name
   this.currentScore = 0 ; //current score of the user
 
   //methods
-  this.requestFirstName = function (readline){
+  this.requestFirstName = function (readline){ //User object asks for a name
         console.log("\nPlease enter a FIRST NAME to continue");
         readline.setPrompt(" First Name >> ");
         readline.prompt();
     }
 };
-
-
 
 module.exports =  User ;
 
